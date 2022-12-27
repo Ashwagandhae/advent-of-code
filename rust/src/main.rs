ea267b298eb660f5f06e2bda0daa25e4ce8838b1
Ashwagandhae <julianlianbauer@gmail.com>
2022-12-27 08:24:23 +0100

    day 10.2 & 11

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,64 +1,217 @@
+use std::collections::{HashMap, VecDeque};
 // import file libraries
 use std::fs::File;
 use std::io::Read;
 
+#[derive(Clone)]
+struct Number {
+    // hashmap
+    mod_dict: HashMap<i128, i128>,
+}
+impl Number {
+    fn new(val: i128) -> Number {
+        let nums = vec![2, 3, 5, 7, 11, 13, 17, 19];
+        let mut mod_dict = HashMap::new();
+        for num in nums {
+            mod_dict.insert(num, val % num);
+        }
+        Number { mod_dict }
+    }
+    fn is_divisible_by(&self, x: &i128) -> bool {
+        self.mod_dict.get(x).unwrap() == &0
+    }
+    fn mult(&self, val: i128) -> Number {
+        // loop thru dict
+        let mut new_num = self.clone();
+        for (key, value) in self.mod_dict.iter() {
+            // multiply value by val
+            new_num.mod_dict.insert(*key, (value * val) % key);
+        }
+        new_num
+    }
+    fn add(&self, val: i128) -> Number {
+        // loop thru dict
+        let mut new_num = self.clone();
+        for (key, value) in self.mod_dict.iter() {
+            // add value by val
+            new_num.mod_dict.insert(*key, (value + val) % key);
+        }
+        new_num
+    }
+    fn square(&self) -> Number {
+        // loop thru dict
+        let mut new_num = self.clone();
+        for (key, value) in self.mod_dict.iter() {
+            // square value
+            new_num.mod_dict.insert(*key, (value * value) % key);
+        }
+        new_num
+    }
+}
+#[derive(Clone)]
+enum Operation {
+    Add(i128),
+    Multiply(i128),
+    Square,
+}
+impl Operation {
+    fn apply(&self, x: &Number) -> Number {
+        match self {
+            Operation::Add(y) => x.add(*y),
+            Operation::Multiply(y) => x.mult(*y),
+            Operation::Square => x.square(),
+        }
+    }
+}
+#[derive(Clone)]
+struct Test {
+    denominator: i128,
+    true_target: i128,
+    false_target: i128,
+}
+impl Test {
+    fn get_target_monkey(&self, x: &Number) -> i128 {
+        match x.is_divisible_by(&self.denominator) {
+            true => self.true_target,
+            false => self.false_target,
+        }
+    }
+}
+#[derive(Clone)]
+struct Monkey {
+    items: VecDeque<Number>,
+    operation: Operation,
+    test: Test,
+    inspect_count: i128,
+}
+impl Monkey {
+    fn new(items: VecDeque<Number>, operation: Operation, test: Test) -> Monkey {
+        Monkey {
+            items,
+            operation,
+            test,
+            inspect_count: 0,
+        }
+    }
+    fn add_item(&mut self, item: Number) {
+        self.items.push_back(item);
+    }
+    fn get_changes(&mut self) -> Vec<(i128, Number)> {
+        self.items
+            .iter()
+            .map(|item| {
+                let inspected_item = self.operation.apply(item);
+                self.inspect_count += 1;
+                let target_monkey = self.test.get_target_monkey(&inspected_item);
+                (target_monkey, inspected_item)
+            })
+            .collect()
+    }
+}
+
 fn main() {
     // open 1.txt
-    let mut file = File::open("../data/8.txt").unwrap();
+    let mut file = File::open("../data/11.txt").unwrap();
     // read file
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    // convert into a 2d array
-    let mut grid: Vec<Vec<i32>> = Vec::new();
-    for line in contents.lines() {
-        let mut row: Vec<i32> = Vec::new();
-        // convert each char to num
-        for c in line.chars() {
-            row.push(c.to_digit(10).unwrap().try_into().unwrap());
-        }
-        grid.push(row);
-    }
-    // save grid dims
-    let grid_width = grid[0].len();
-    let grid_height = grid.len();
 
-    let mut max_tree_view = 0;
-    // find best tree
-    for y in 0..grid_height {
-        for x in 0..grid_width {
-            let curr = grid[y][x];
-            // get up view from current pos
-            let mut view_dist = 1;
-            let dirs: Vec<(i32, i32)> = vec![(0, 1), (0, -1), (1, 0), (-1, 0)];
-            for (delta_x, delta_y) in dirs {
-                let (mut move_y, mut move_x): (i32, i32) =
-                    (y.try_into().unwrap(), x.try_into().unwrap());
-
-                let mut count: i32 = 0;
-                loop {
-                    move_y += delta_y;
-                    move_x += delta_x;
-                    if move_y < 0
-                        || move_y >= grid_height as i32
-                        || move_x < 0
-                        || move_x >= grid_width as i32
-                    {
-                        break;
-                    }
-                    count += 1;
-                    let check = grid[move_y as usize][move_x as usize];
-                    println!("({} {}) = {}", move_x, move_y, check);
-                    if check >= curr {
-                        break;
-                    }
+    let mut monkeys: Vec<Monkey> = contents
+        .split("\n\n")
+        .map(|block| {
+            let mut lines = block.lines();
+            let items: VecDeque<Number> = lines
+                .nth(1)
+                .unwrap()
+                .split(": ")
+                .nth(1)
+                .unwrap()
+                .split(", ")
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<i128>>()
+                .iter()
+                .map(|x| Number::new(*x))
+                .collect();
+            let operation = match lines
+                .nth(0)
+                .unwrap()
+                .split("Operation: new = old ")
+                .nth(1)
+                .unwrap()
+            {
+                "* old" => Operation::Square,
+                x if x.contains("+") => {
+                    Operation::Add(x.split("+ ").nth(1).unwrap().parse().unwrap())
                 }
-                println!("{}", count);
-                view_dist *= count;
+                x if x.contains("*") => {
+                    Operation::Multiply(x.split("* ").nth(1).unwrap().parse().unwrap())
+                }
+                _ => panic!("Unknown operation"),
+            };
+            let test_cond: i128 = lines
+                .nth(0)
+                .unwrap()
+                .split("Test: divisible by ")
+                .nth(1)
+                .unwrap()
+                .parse()
+                .unwrap();
+            let true_monkey: i128 = lines
+                .nth(0)
+                .unwrap()
+                .split("If true: throw to monkey ")
+                .nth(1)
+                .unwrap()
+                .parse()
+                .unwrap();
+            let false_monkey: i128 = lines
+                .nth(0)
+                .unwrap()
+                .split("If false: throw to monkey ")
+                .nth(1)
+                .unwrap()
+                .parse()
+                .unwrap();
+
+            Monkey::new(
+                items,
+                operation,
+                Test {
+                    denominator: test_cond,
+                    true_target: true_monkey,
+                    false_target: false_monkey,
+                },
+            )
+        })
+        .collect();
+    for round in 0..10000 {
+        println!("round {}", round);
+
+        for j in 0..monkeys.len() {
+            // print monkey items
+            for (target_monkey, relief_item) in monkeys[j].get_changes() {
+                monkeys[target_monkey as usize].add_item(relief_item);
             }
-            if view_dist > max_tree_view {
-                max_tree_view = view_dist;
+            monkeys[j].items.clear();
+        }
+        // print each monkey item in each round
+        println!("{}", monkeys.len());
+        for (i, monkey) in monkeys.iter().enumerate() {
+            for item in monkey.items.iter() {
+                // println!("Monkey {} has item {}", i, item);
             }
         }
     }
-    println!("{}", max_tree_view);
+    println!("got thru");
+    // sort monkeys by inspect_count (largest first)
+    monkeys.sort_by(|a, b| b.inspect_count.cmp(&a.inspect_count));
+    // print top two
+    for monkey in monkeys.iter().take(2) {
+        println!("Monkey {} ", monkey.inspect_count,);
+    }
+    // multiply top two
+    println!(
+        "Product of top two monkeys: {}",
+        monkeys[0].inspect_count * monkeys[1].inspect_count
+    );
 }
