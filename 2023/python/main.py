924b3f6da414f500a7a008576de71769dc550224
Ashwagandhae <julianlianbauer@gmail.com>
2023-12-04 18:28:11 -0800

    delete inputs

--- 2023/python/main.py
+++ 2023/python/main.py
@@ -2,20 +2,47 @@ s = ""
 with open("./input.txt", "r") as f:
     s = f.read()
 
-import time
 
-for i in range(0, 5):
-    print(i)
-    time.sleep(1)
-
-raise Exception("This is a test exception")
+arr = s.split("\n")
 
 
-def num(l):
-    digits = [c for c in l if c in list("1234567890")]
-    return int(digits[0] + digits[-1])
+def get(x, y):
+    if x < 0 or y < 0:
+        return "."
+    if y >= len(arr):
+        return "."
+    if x >= len(arr[y]):
+        return "."
+    return arr[y][x]
 
 
-digits = [num(l) for l in s.split("\n")]
+def look_around(x, y):
+    return [
+        get(x - 1, y - 1),
+        get(x, y - 1),
+        get(x + 1, y - 1),
+        get(x - 1, y),
+        get(x + 1, y),
+        get(x - 1, y + 1),
+        get(x, y + 1),
+        get(x + 1, y + 1),
+    ]
 
-print(sum(digits))
+
+num = "1234567890"
+sum = 0
+for y in range(len(arr)):
+    for x in range(len(arr[0])):
+        if get(x, y) in num and get(x - 1, y) not in num:
+            num_parts = [(x, y)]
+            new_x = x + 1
+            while get(new_x, y) in num:
+                num_parts.append((new_x, y))
+                new_x += 1
+            look_around_parts = []
+            for x, y in num_parts:
+                look_around_parts += look_around(x, y)
+            if any([l != "." and l not in num for l in look_around_parts]):
+                sum += int("".join([get(x, y) for x, y in num_parts]))
+
+print(sum)
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,11 +1,32 @@
-#![allow(unused_imports)]
-use aoc_parse::{parser, prelude::*};
-use itertools::Itertools;
-use std::collections::{HashMap, HashSet, VecDeque};
-use std::fs::read_to_string;
+use std::fs::{read_to_string, File};
 
 fn main() {
     let txt = read_to_string("./input.txt").unwrap();
-    let answer = parser!(lines(i32)).parse(&txt).unwrap();
-    println!("{:?}", answer);
+    let answer: usize = txt
+        .split("\n")
+        .map(|line| {
+            let (_, line) = line.split_once(": ").unwrap();
+            let (winning, real) = line.split_once(" | ").unwrap();
+            let winning = winning
+                .split_whitespace()
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            let real = real
+                .split_whitespace()
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            let mut score = 0;
+            for r in real {
+                if winning.contains(&r) {
+                    if score == 0 {
+                        score = 1;
+                    } else {
+                        score *= 2;
+                    }
+                }
+            }
+            score
+        })
+        .sum();
+    println!("{}", answer);
 }
